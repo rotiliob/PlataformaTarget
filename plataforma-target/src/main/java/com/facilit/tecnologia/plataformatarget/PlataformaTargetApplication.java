6862a75808d6de8e08806c5d234bb345d85959a6
Andre Burgos <rotiliob@gmail.com>
2020-02-18 20:30:19 -0300

    Add Project in git

---
+++ plataforma-target/src/main/java/com/facilit/tecnologia/plataformatarget/PlataformaTargetApplication.java
@@ -0,0 +1,13 @@
+package com.facilit.tecnologia.plataformatarget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaTargetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaTargetApplication.class, args);
+	}
+
+}
